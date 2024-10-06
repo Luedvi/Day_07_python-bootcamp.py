db38a2113df3b8abcc13bcdaac2dc227506174c1
Luedvi <luedvi@outlook.com>
2024-10-05 21:43:02 -0600

    Update main.py final

--- main.py
+++ main.py
@@ -1,56 +1,42 @@
-# Step 5
-from replit import clear
-import random
-
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-# Delete this line: word_list = ["aardvark", "baboon", "camel"]
-from hangman_words import word_list
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
-game_over = False
-lives = 6
-
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo, stages
-print(logo)
-# Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+from random import choice
+from hangman_words import spanish_word_list, clues
+from replit import clear
 
-# Create blanks
+chosen_word = choice(spanish_word_list)
+clue_index = spanish_word_list.index(chosen_word)
+word_length = len(chosen_word)
 display = []
-for _ in range(word_length):
+game_over = False
+lives = len(stages) - 1
+for letter in chosen_word:
     display += "_"
 
+print(logo)
+
 while not game_over:
-    guess = input("Guess a letter: ").lower()
-    # Use the clear() function imported from replit to clear the screen output between guesses.
+    guess = input("Adivina la palabra secreta. Escribe una letra o escribe 'pista' para ver información de la palabra: ").lower()
     clear()
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
     if guess in display:
-        print("You've already guessed", guess)
-    # Check guessed letter
+        print("ya adivinaste esta letra")
+
     for index in range(word_length):
-        letter = chosen_word[index]
-        # print(f"Current position: {index}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[index] = letter
+        if chosen_word[index] == guess:
+            display[index] = guess
+            if "_" not in display:  # can be a nested "if" or not, it doesn't affect the outcome
+                game_over = True
+                print("¡Felicidades! adivinaste la palabra secreta")
 
-    # Check if user is wrong.
-    if guess not in chosen_word:
-        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    print(" ".join(display))
+
+    if guess == "pista":
+        print("pista: ", clues[clue_index])
+    elif guess not in chosen_word:
         lives -= 1
-        print(f"You guessed {guess}, that's not in the word. You lose a life.")
-        if lives == 0:
+        if lives == 0:  # can be a nested "if" or not, it doesn't affect the outcome
             game_over = True
-            print("You lose.")
-
-    # Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
-
-    # Check if user has got all letters.
-    if "_" not in display:
-        game_over = True
-        print("You win.")
-
-    # TODO-2: - Import the stages from hangman_art.py and make this error go away.
+            print("Se te acabaron las oportunidades. Has perdido el juego")
+        else:
+            print(f"La letra {guess} no se encuentra en la palabra secreta. Tienes una oportunidad menos")
     print(stages[lives])
